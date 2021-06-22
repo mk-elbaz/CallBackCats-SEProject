f1a7ee4d30b82bb747971a1e8f84f277c5a9bdc8
mk-elbaz <66201789+mk-elbaz@users.noreply.github.com>
2021-06-22 21:04:59 +0200

    a

--- client/src/Components/ChangePassword.js
+++ client/src/Components/ChangePassword.js
@@ -1,7 +1,26 @@
 import React from "react";
 
+<<<<<<< HEAD
 const changePass = () => (
   <h1>Change Pass Page </h1>
 )
+=======
+const changePass = () => {
+  /*<div>
+    <form onSubmit={onSubmit}>
+      <h1>Change Password</h1>
+      <input
+        type="password"
+        name="password"
+        value={user.password}
+        onChange={onChange}
+        className="form-control"
+        placeholder="Enter Password"
+      />
+    </form>
+    {message ? <Message message={message} /> : null}
+  </div>*/
+};
+>>>>>>> parent of 1d84d79 (Revert "weee")
 
 export default changePass;
