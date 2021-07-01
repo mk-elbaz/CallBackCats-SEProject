53974e41978802f882bf7dc11dd0e8e6c27f8d21
Abdelrahman Hany <34286982+Karitori@users.noreply.github.com>
2021-07-01 23:56:55 +0200

    final

--- client/src/Components/createschedule.js
+++ client/src/Components/createschedule.js
@@ -125,57 +125,57 @@ export default function Createschedule() {
           className="form-control"
           placeholder="Enter The Faculty"
         />
-        <label htmlFor="day1" className="sr-only">
+        <label htmlFor="day" className="sr-only">
           day1:{" "}
         </label>
         <input
           type="text"
-          name="day1"
-          value={schedule.day1}
+          name="day"
+          value={schedule.day}
           onChange={onChange}
           className="form-control"
           placeholder="Enter The Day"
         />
         <label htmlFor="first1" className="sr-only">
-          first1:{" "}
+          first:{" "}
         </label>
         <input
           type="text"
-          name="first1"
-          value={schedule.first1}
+          name="first"
+          value={schedule.first}
           onChange={onChange}
           className="form-control"
           placeholder="Enter First Slot"
         />
-         <label htmlFor="second1" className="sr-only">
+         <label htmlFor="second" className="sr-only">
           second1:{" "}
         </label>
         <input
           type="text"
-          name="second1"
-          value={schedule.second1}
+          name="second"
+          value={schedule.second}
           onChange={onChange}
           className="form-control"
           placeholder="Enter Second Slot"
         />
-         <label htmlFor="third1" className="sr-only">
+         <label htmlFor="third" className="sr-only">
           third1:{" "}
         </label>
         <input
           type="text"
-          name="third1"
-          value={schedule.third1}
+          name="third"
+          value={schedule.third}
           onChange={onChange}
           className="form-control"
           placeholder="Enter Third Slot"
         />
          <label htmlFor="fourth1" className="sr-only">
-          fourth1:{" "}
+          fourth:{" "}
         </label>
         <input
           type="text"
-          name="fourth1"
-          value={schedule.fourth1}
+          name="fourth"
+          value={schedule.fourth}
           onChange={onChange}
           className="form-control"
           placeholder="Enter Fourth Slot"
@@ -183,245 +183,6 @@ export default function Createschedule() {
 
         <p></p>
         <p></p>
-
-<label htmlFor="day2" className="sr-only">
-          day2:{" "}
-        </label>
-        <input
-          type="text"
-          name="day2"
-          value={schedule.day2}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter The Day"
-        />
-        <label htmlFor="first2" className="sr-only">
-          first2:{" "}
-        </label>
-        <input
-          type="text"
-          name="first2"
-          value={schedule.first2}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter First Slot"
-        />
-         <label htmlFor="second2" className="sr-only">
-          second2:{" "}
-        </label>
-        <input
-          type="text"
-          name="second2"
-          value={schedule.second2}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Second Slot"
-        />
-         <label htmlFor="third2" className="sr-only">
-          third2:{" "}
-        </label>
-        <input
-          type="text"
-          name="third2"
-          value={schedule.third2}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Third Slot"
-        />
-         <label htmlFor="fourth2" className="sr-only">
-          fourth2:{" "}
-        </label>
-        <input
-          type="text"
-          name="fourth2"
-          value={schedule.fourth2}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Fourth Slot"
-        />
-
-        <p></p>
-        <p></p>
-
-<label htmlFor="day3" className="sr-only">
-          day3:{" "}
-        </label>
-        <input
-          type="text"
-          name="day3"
-          value={schedule.day3}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter The Day"
-        />
-        <label htmlFor="first3" className="sr-only">
-          first3:{" "}
-        </label>
-        <input
-          type="text"
-          name="first3"
-          value={schedule.first3}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter First Slot"
-        />
-         <label htmlFor="second3" className="sr-only">
-          second3:{" "}
-        </label>
-        <input
-          type="text"
-          name="second3"
-          value={schedule.second3}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Second Slot"
-        />
-         <label htmlFor="third3" className="sr-only">
-          third3:{" "}
-        </label>
-        <input
-          type="text"
-          name="third3"
-          value={schedule.third3}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Third Slot"
-        />
-         <label htmlFor="fourth3" className="sr-only">
-          fourth3:{" "}
-        </label>
-        <input
-          type="text"
-          name="fourth3"
-          value={schedule.fourth3}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Fourth Slot"
-        />
-
-        <p></p>
-        <p></p>
-
-
-<label htmlFor="day4" className="sr-only">
-          day4:{" "}
-        </label>
-        <input
-          type="text"
-          name="day4"
-          value={schedule.day4}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter The Day"
-        />
-        <label htmlFor="first4" className="sr-only">
-          first4:{" "}
-        </label>
-        <input
-          type="text"
-          name="first4"
-          value={schedule.first4}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter First Slot"
-        />
-         <label htmlFor="second4" className="sr-only">
-          second4:{" "}
-        </label>
-        <input
-          type="text"
-          name="second4"
-          value={schedule.second4}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Second Slot"
-        />
-         <label htmlFor="third4" className="sr-only">
-          third4:{" "}
-        </label>
-        <input
-          type="text"
-          name="third4"
-          value={schedule.third4}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Third Slot"
-        />
-         <label htmlFor="fourth4" className="sr-only">
-          fourth4:{" "}
-        </label>
-        <input
-          type="text"
-          name="fourth4"
-          value={schedule.fourth4}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Fourth Slot"
-        />
-
-        <p></p>
-        <p></p>
-
-<label htmlFor="day5" className="sr-only">
-          day5:{" "}
-        </label>
-        <input
-          type="text"
-          name="day5"
-          value={schedule.day5}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter The Day"
-        />
-        <label htmlFor="first5" className="sr-only">
-          first5:{" "}
-        </label>
-        <input
-          type="text"
-          name="first5"
-          value={schedule.first5}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter First Slot"
-        />
-         <label htmlFor="second5" className="sr-only">
-          second5:{" "}
-        </label>
-        <input
-          type="text"
-          name="second5"
-          value={schedule.second5}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Second Slot"
-        />
-         <label htmlFor="third5" className="sr-only">
-          third5:{" "}
-        </label>
-        <input
-          type="text"
-          name="third5"
-          value={schedule.third5}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Third Slot"
-        />
-         <label htmlFor="fourth5" className="sr-only">
-          fourth5:{" "}
-        </label>
-        <input
-          type="text"
-          name="fourth5"
-          value={schedule.fourth5}
-          onChange={onChange}
-          className="form-control"
-          placeholder="Enter Fourth Slot"
-        />
-
-        <p></p>
-        <p></p>
-         
-        
         
         <button className="btn btn-lg btn-primary btn-block" type="submit">
           Add Schedule
--- models/schedule.js
+++ models/schedule.js
@@ -1,38 +1,13 @@
 const mongoose = require('mongoose');
 
 const scheduleSchema = mongoose.Schema({
-    faculty : String,
-    first1: String,
-    second1: String,
-    third1: String,
-    fourth1: String,
-    day1: String,
-
-    first2: String,
-    second2: String,
-    third2: String,
-    fourth2: String,
-    day2: String,
-
-    first3: String,
-    second3: String,
-    third3: String,
-    fourth3: String,
-    day3: String,
-
-    first4: String,
-    second4: String,
-    third4: String,
-    fourth4: String,
-    day4: String,
-
-    first5: String,
-    second5: String,
-    third5: String,
-    fourth5: String,
-    day5: String,
-
-
+    first: String,
+    second: String,
+    third: String,
+    fourth: String,
+    day: String,
+    faculty : String
+    
 
 });
 
